84bb72c311ef90aaa8628f2a192a0fbe6a44f130
Robert Pjetri <rpjetri91@gmail.com>
2023-04-11 22:48:10 -0600

    Async Campsites

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
   return (
+
     <div className="App">
       <Header />
       <Routes>
